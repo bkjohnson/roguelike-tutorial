8bbdd3d505d8ef2fbf19a45d1a8f553174581873
Brooks Johnson <brooks.johnson88@gmail.com>
2017-07-02 17:35:37 -0500

    Draw '@' & handle basic input

--- src/core/Game.js
+++ src/core/Game.js
@@ -1,6 +1,27 @@
 class Game {
   constructor() {
     this.display = new ROT.Display()
+    this.agents = []
+
     document.body.appendChild(this.display.getContainer())
+  }
+
+  addAgent(agent) {
+    this.agents.push(agent)
+
+    this.display.draw( agent._x, agent._y, agent._char, "#fff" )
+  }
+
+  start() {
+    window.requestAnimationFrame(this.start.bind(this))
+    this.updateDisplay()
+  }
+
+  updateDisplay() {
+    let agent = null
+    for (let i = 0; i < this.agents.length; i++) {
+      agent = this.agents[i]
+      this.display.draw( agent._x, agent._y, agent._char )
+    }
   }
 }
